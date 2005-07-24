ff24eccd67ce2139d26ed11a0286e3d31794dfe6
Tanaka Akira <akr@fsij.org>
2005-07-24 18:16:20 +0000

    don't add a key to known_hosts if it is already known.

--- ssh.rb
+++ ssh.rb
@@ -7,14 +7,20 @@ module SSH
     when Array
       arg.each {|a| add_ssh_known_host(a) }
     when String
-      host = arg[/\A[^ ,]+/]
+      hostnames, type, key_rest = arg.split(/\s+/, 3)
+      hostnames = hostnames.split(/,/)
       begin
         Dir.mkdir("#{HOME_DIRECTORY}/.ssh", 0700)
       rescue Errno::EEXIST
       end
       open("#{HOME_DIRECTORY}/.ssh/known_hosts", File::RDWR|File::CREAT) {|f|
         f.each_line {|line|
-	  return if line == arg
+          hs, t, kr = line.split(/\s+/, 3)
+          next if t != type
+          hs = hs.split(/,/)
+          if !(hs & hostnames).empty?
+            return
+          end
         }
         f.puts arg
       }
