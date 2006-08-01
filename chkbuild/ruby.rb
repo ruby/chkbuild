0b9adbafac09e35c84ffdcf4b315d8ba25a8ee91
Tanaka Akira <akr@fsij.org>
2006-08-01 03:57:40 +0000

    sample/build-ruby2 use chkbuild/ruby.rb.

--- chkbuild/ruby.rb
+++ chkbuild/ruby.rb
@@ -1,8 +1,18 @@
 require 'build'
+require 'pathname'
 
 def build_ruby(*args)
+  build_ruby_internal(false, *args)
+end
+
+def build_ruby2(*args)
+  build_ruby_internal(true, *args)
+end
+
+def build_ruby_internal(separated_dir, *args)
   Build.perm_target("ruby", *args) {
       |ruby_curr_dir, concatenated_suffix, *suffixes|
+    ruby_curr_dir = Pathname.new(ruby_curr_dir)
 
     ruby_branch = nil
     configure_flags = []
@@ -16,13 +26,25 @@ def build_ruby(*args)
       end
     }
 
+    objdir = ruby_curr_dir+'ruby'
+    if separated_dir
+      checkout_dir = ruby_curr_dir.dirname
+    else
+      checkout_dir = ruby_curr_dir
+    end
+    srcdir = (checkout_dir+'ruby').relative_path_from(objdir)
+
+    Dir.chdir(checkout_dir)
     Build.cvs(
       ":pserver:anonymous@cvs.ruby-lang.org:/src", "ruby", ruby_branch,
       :cvsweb => "http://www.ruby-lang.org/cgi-bin/cvsweb.cgi"
       )
     Dir.chdir("ruby")
     Build.run("autoconf")
-    Build.run("./configure", "--prefix=#{ruby_curr_dir}", "CFLAGS=-Wall -Wformat=2 -Wno-parentheses -g -O2 -DRUBY_GC_STRESS #{cflags}", *configure_flags) {|log|
+
+    Dir.chdir(ruby_curr_dir)
+    Build.mkcd("ruby")
+    Build.run("#{srcdir}/configure", "--prefix=#{ruby_curr_dir}", "CFLAGS=-Wall -Wformat=2 -Wno-parentheses -g -O2 -DRUBY_GC_STRESS #{cflags}", *configure_flags) {|log|
       if /^checking target system type\.\.\. (\S+)$/ =~ log
         Build.update_title(:version, "#{['ruby', *suffixes].join('-')} #{$1}")
       end
@@ -43,7 +65,7 @@ def build_ruby(*args)
       end
     }
     Build.make("install")
-    Build.run("./ruby", "sample/test.rb", :section=>"test.rb") {|log|
+    Build.run("./ruby", "#{srcdir+'sample/test.rb'}", :section=>"test.rb") {|log|
       Build.update_title(:status) {|val|
         if !val
           if /^end of test/ !~ log
@@ -56,7 +78,7 @@ def build_ruby(*args)
         end
       }
     }
-    Build.run("./ruby", "test/runner.rb", "-v", :section=>"test-all") {|log|
+    Build.run("./ruby", "#{srcdir+'test/runner.rb'}", "-v", :section=>"test-all") {|log|
       Build.update_title(:status) {|val|
         if !val
           if /^\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors$/ =~ log
