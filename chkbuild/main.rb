4f7ec4fbf26027f4aa7f9e8f8e94547f4a916919
Tanaka Akira <akr@fsij.org>
2006-08-06 04:04:06 +0000

    help subcommand implemented.

--- chkbuild/main.rb
+++ chkbuild/main.rb
@@ -5,6 +5,23 @@ module ChkBuild
   def ChkBuild.build_dir() Pathname.new("#{TOP_DIRECTORY}/tmp/build") end
   def ChkBuild.public_dir() Pathname.new("#{TOP_DIRECTORY}/tmp/public_html") end
 
+  def ChkBuild.main_help(status=true)
+    if File.executable? $0
+      command = $0
+    else
+      require 'rbconfig'
+      ruby = "#{Config::CONFIG["bindir"]}/#{Config::CONFIG["ruby_install_name"]}"
+      command = "#{ruby} #{$0}"
+    end
+    print <<"End"
+usage:
+  #{command} [build]
+  #{command} list
+  #{command} title
+End
+    exit status
+  end
+
   @target_list = []
   def ChkBuild.main_build
     ::Build.lock_start
@@ -42,13 +59,12 @@ module ChkBuild
   end
 
   def ChkBuild.main
-    case subcommand = ARGV[0]
-    when 'build', nil
-      ChkBuild.main_build
-    when 'list'
-      ChkBuild.main_list
-    when 'title'
-      ChkBuild.main_title
+    subcommand = ARGV[0] || 'build'
+    case subcommand
+    when 'help', '-h' then ChkBuild.main_help
+    when 'build' then ChkBuild.main_build
+    when 'list' then ChkBuild.main_list
+    when 'title' then ChkBuild.main_title
     else
       puts "unexpected subcommand: #{subcommand}"
       exit 1
