38f716ca4a046219a5748456f87955eb24d2f462
Tanaka Akira <akr@fsij.org>
2006-08-11 11:50:20 +0000

    use ChkBuild.main instead of Build.main. Build.limit removed. Build::TOP_DIRECTORY removed.

--- build.rb
+++ build.rb
@@ -14,16 +14,9 @@ STDIN.reopen("/dev/null", "r")
 STDOUT.sync = true
 
 class Build
-  def Build.main() ChkBuild.main end
   def Build.def_target(target_name, *args, &block)
     ChkBuild.def_target(target_name, *args, &block)
   end
-
-  def self.limit(hash)
-    ChkBuild.limit(hash)
-  end
-
-  TOP_DIRECTORY = Dir.getwd
 
   ChkBuild.build_top.mkpath
 end
