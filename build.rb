cf8f0d7666c9d01234d4a5ca07d2c9f8d7732083
Tanaka Akira <akr@fsij.org>
2006-08-12 05:57:20 +0000

    make build.rb to contain only one "require".

--- build.rb
+++ build.rb
@@ -1,5 +1,1 @@
 require 'chkbuild'
-
-require "util"
-require 'chkbuild/target'
-require 'chkbuild/build'
--- chkbuild.rb
+++ chkbuild.rb
@@ -3,3 +3,6 @@ require 'chkbuild/lock'
 require 'chkbuild/cvs'
 require 'chkbuild/svn'
 require 'chkbuild/xforge'
+require "util"
+require 'chkbuild/target'
+require 'chkbuild/build'
