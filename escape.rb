393a6aa495f32bf92ca6e2312de6d1cb5167d21b
Tanaka Akira <akr@fsij.org>
2006-08-20 12:26:43 +0000

    comment updated.

--- escape.rb
+++ escape.rb
@@ -20,7 +20,7 @@ module Escape
   end
 
   def uri_segment(str)
-    # pchar : unreserved / sub-delims / ":" / "@"
+    # pchar - pct-encoded = unreserved / sub-delims / ":" / "@"
     # unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~"
     # sub-delims = "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
     str.gsub(%r{[^A-Za-z0-9\-._~!$&'()*+,;=:@]}) {
@@ -34,11 +34,13 @@ module Escape
 
   def html_form(pairs, sep=';')
     pairs.map {|k, v|
-      # query chars - x-www-form-urlencoded delimiters
-      # query chars: unreserved / sub-delims / "/" / ":" / "?" / "@"
+      # query-chars - pct-encoded - x-www-form-urlencoded-delimiters =
+      #   unreserved / "!" / "$" / "'" / "(" / ")" / "*" / "," / ":" / "@" / "/" / "?"
+      # query-char - pct-encoded = unreserved / sub-delims / ":" / "@" / "/" / "?"
+      # query-char = pchar / "/" / "?" = unreserved / pct-encoded / sub-delims / ":" / "@" / "/" / "?"
       # unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~"
       # sub-delims = "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
-      # x-www-form-urlencoded delimiters: "&" / "+" / ";" / "="
+      # x-www-form-urlencoded-delimiters = "&" / "+" / ";" / "="
       k = k.gsub(%r{[^0-9A-Za-z\-\._~:/?@!\$'()*,]}) {
         '%' + $&.unpack("H2")[0].upcase
       }
