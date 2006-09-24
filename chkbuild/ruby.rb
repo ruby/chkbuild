844645d617948786c6415eecb9b5bcf6e0db24b8
Tanaka Akira <akr@fsij.org>
2006-09-24 06:50:04 +0000

    refined again.

--- chkbuild/ruby.rb
+++ chkbuild/ruby.rb
@@ -159,8 +159,8 @@ End
       }
 
       # [2006-09-24T12:48:49.245737 #6902] ERROR -- : undefined method `each' for #<String:0x447fc5e4> (NoMethodError)
-      t.add_diff_preprocess_gsub(%r{\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)}o) {|match|
-        "YYYY-MM-DDThh:mm:ss" + match[1].gsub(/\d/, 's')
+      t.add_diff_preprocess_gsub(%r{\[\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+) \#(\d+)\]}o) {|match|
+        "[YYYY-MM-DDThh:mm:ss" + match[1].gsub(/\d/, 's') + " #" + match[2].gsub(/\d/, 'd') + "]"
       }
 
       # #<String:0x4455ae94
