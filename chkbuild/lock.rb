c3f865ae95bdbd8a1e2d6b3b8294cd3182f53715
Tanaka Akira <akr@fsij.org>
2009-10-14 23:12:48 +0000

    clear .lock when it is more than 10K.

--- chkbuild/lock.rb
+++ chkbuild/lock.rb
@@ -29,18 +29,20 @@ module ChkBuild
 
   def self.lock_start
     if !defined?(@lock_io)
-      @lock_io = LOCK_PATH.open(File::WRONLY|File::CREAT)
+      @lock_io = LOCK_PATH.open(File::WRONLY|File::CREAT|File::APPEND)
     end
     if @lock_io.flock(File::LOCK_EX|File::LOCK_NB) == false
       raise "another chkbuild is running."
     end
-    @lock_io.truncate(0)
+    if 10240 < @lock_io.stat.size
+      @lock_io.truncate(0)
+    end
     @lock_io.sync = true
     @lock_io.close_on_exec = true
     @lock_io.puts "locked pid:#{$$}"
     lock_pid = $$
     at_exit {
-      @lock_io.puts "exit pid:#{$$}" if $$ == lock_pid
+      @lock_io.print "exit pid:#{$$}\n\n" if $$ == lock_pid
     }
   end
 
