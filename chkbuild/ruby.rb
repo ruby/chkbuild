c52aacf412df33b604af0e2713c69930caffc76e
Tanaka Akira <akr@fsij.org>
2010-01-21 05:09:52 +0000

    add copyright year 2010.

--- chkbuild/ruby.rb
+++ chkbuild/ruby.rb
@@ -1,6 +1,6 @@
 # chkbuild/ruby.rb - ruby build module
 #
-# Copyright (C) 2006,2007,2008,2009 Tanaka Akira  <akr@fsij.org>
+# Copyright (C) 2006,2007,2008,2009,2010 Tanaka Akira  <akr@fsij.org>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
