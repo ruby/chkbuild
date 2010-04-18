ea410652611ec02aa6fbf197e895cda80dc446c4
Tanaka Akira <akr@fsij.org>
2010-04-18 12:06:17 +0000

    updated to follow timeoutcom.rb.

--- test/test-timeoutcom.rb
+++ test/test-timeoutcom.rb
@@ -5,19 +5,19 @@ class TestTimeoutCommand < Test::Unit::TestCase
   def test_time
     assert_raise(CommandTimeout) {
       open("/dev/null", 'w') {|null|
-        TimeoutCommand.timeout_command(Time.now+1, null) {
+        TimeoutCommand.timeout_command(<<-'End', Time.now+1, null)
           begin
             sleep 2
           rescue Interrupt
           end
-        }
+        End
       }
     }
   end
 
   def test_past_time
     assert_raise(CommandTimeout) {
-      TimeoutCommand.timeout_command(Time.now-1, nil) {}
+      TimeoutCommand.timeout_command('', Time.now-1, nil)
     }
   end
 end
