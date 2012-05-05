c1adbd873c295e7108417fa8c3c52187ca4dd9b4
Tanaka Akira <akr@fsij.org>
2012-05-05 15:15:00 +0900

    don't show number of skips.

--- chkbuild/ruby.rb
+++ chkbuild/ruby.rb
@@ -518,13 +518,8 @@ ChkBuild.define_failure_hook('ruby', "test-all") {|log|
   elsif /.*^\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors, (\d+) skips$/m =~ log
     failures = $1.to_i
     errors = $2.to_i
-    skips = $3.to_i
-    if failures != 0 || errors != 0 || skips != 0
-      if skips == 0
-	"#{failures}F#{errors}E"
-      else
-	"#{failures}F#{errors}E#{skips}S"
-      end
+    if failures != 0 || errors != 0
+      "#{failures}F#{errors}E"
     end
   end
 }
