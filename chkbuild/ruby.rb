5b8d38f1fc35b66afd9c5ffbdd4b10a0452dede6
Benoit Daloze <eregontp@gmail.com>
2015-02-25 10:56:55 +0100

    Use the ruby.mspec config file for recent RubySpec.
    * See https://github.com/ruby/rubyspec/commit/7a909e925c1baa9c700bd44af9241aef6e596714
      and https://github.com/ruby/rubyspec/commit/c750ce7b6d876ac21bdf477104072fea5331a697

--- chkbuild/ruby.rb
+++ chkbuild/ruby.rb
@@ -597,11 +597,7 @@ def (ChkBuild::Ruby).build_proc(b)
       excludes = ["rubyspec/optional/ffi"]
       b.catch_error {
         FileUtils.rmtree "rubyspec_temp"
-        if ruby_version.before(1,9)
-          config = Dir.pwd + "/rubyspec/ruby.1.8.mspec"
-        else
-          config = Dir.pwd + "/rubyspec/ruby.1.9.mspec"
-        end
+        config = Dir.pwd + "/rubyspec/ruby.mspec"
         command = %W[bin/ruby mspec/bin/mspec -V -f s -B #{config} -t #{rubybin}]
         command << "rubyspec"
         command << {
@@ -619,11 +615,7 @@ def (ChkBuild::Ruby).build_proc(b)
             next if !s.file?
             b.catch_error {
               FileUtils.rmtree "rubyspec_temp"
-              if ruby_version.before(1,9)
-                config = ruby_build_dir + "rubyspec/ruby.1.8.mspec"
-              else
-                config = ruby_build_dir + "rubyspec/ruby.1.9.mspec"
-              end
+              config = ruby_build_dir + "rubyspec/ruby.mspec"
               command = %W[bin/ruby mspec/bin/mspec -V -f s -B #{config} -t #{rubybin}]
               command << f.to_s
               command << {
